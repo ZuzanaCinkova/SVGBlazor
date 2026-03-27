8fb740fee4ed3202817accb9713ed05a8438be75
xcinz100 <xcinz100@KL213P05-KVD>
2026-03-27 14:33:06 +0100

    Přidejte soubory projektu.

---
+++ Models/Tvar.cs
@@ -0,0 +1,18 @@
+﻿namespace SVGBlazor.Models
+{
+    public abstract class Tvar
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public string barva { get; set; } = "blue";
+
+            public void Posun(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+        public abstract void Nakresli();
+    }
+}
